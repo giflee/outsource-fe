36092f2ad850bd14f5a8627506010012b97600a6
jianfulee <jianfulee@hotmail.com>
2017-04-27 19:17:38 +0800

    init project

---
+++ src/pages/user/index/app.vue
@@ -0,0 +1,165 @@
+<template>
+  <div id="app">
+    <el-card class="index-card">
+      <el-button type="primary" @click="gogogo">To Customer Home Page</el-button>
+      <el-button type="primary" plain @click="tototo">To User Login Page</el-button>
+    </el-card>
+
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="title"
+        label="标题"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="owner_name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="owner_tel"
+        label="电话">
+      </el-table-column>
+      <el-table-column prop="old_price" label="价格"></el-table-column>
+      <el-table-column prop="descrition" label="描述"></el-table-column>
+    </el-table>
+    <br>
+    <br>
+     <div class="block">
+        <el-date-picker
+          v-model="value2"
+          align="right"
+          type="date"
+          placeholder="选择日期"
+          :picker-options="pickerOptions1">
+        </el-date-picker>
+      </div>
+      <br>
+      <br>
+       <el-button
+          plain
+          @click="open3">
+          成功
+        </el-button>
+        <el-button
+          plain
+          @click="open4">
+          警告
+        </el-button>
+        <el-button
+          plain
+          @click="open5">
+          消息
+        </el-button>
+        <el-button
+          plain
+          @click="open6">
+          错误
+        </el-button>
+
+        <br>
+        <br>
+        <el-button plain @click="get6">get</el-button>
+        <el-button plain @click="clear6">clear</el-button>
+    </div>
+</template>
+
+<script>
+  import logo from 'assets/img/logo.png'
+  import modal from 'components/modal.vue'
+  export default {
+    components: {
+      modal
+    },
+    data() {
+        return {
+          tableData: [],
+          pickerOptions1: {
+          shortcuts: [{
+            text: '今天',
+            onClick(picker) {
+              picker.$emit('pick', new Date());
+            }
+          }, {
+            text: '昨天',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24);
+              picker.$emit('pick', date);
+            }
+          }, {
+            text: '一周前',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit('pick', date);
+            }
+          }]
+        },
+        value2: ''
+        }
+      },
+    methods: {
+      gogogo () {
+        location.assign('../customer/home.html')
+      },
+      tototo () {
+        location.assign('../user/login.html')
+      },
+       open3() {
+        this.$notify({
+          title: '成功',
+          message: '这是一条成功的提示消息',
+          type: 'success'
+        });
+      },
+
+      open4() {
+        this.$notify({
+          title: '警告',
+          message: '这是一条警告的提示消息',
+          type: 'warning'
+        });
+      },
+
+      open5() {
+        this.$notify.info({
+          title: '消息',
+          message: '这是一条消息的提示消息'
+        });
+      },
+
+      open6() {
+        this.$notify.error({
+          title: '错误',
+          message: '这是一条错误的提示消息'
+        });
+      },
+
+      get6() {
+        var _$$this = this;
+        this.$http.get('/api/cn_goods').then((ret => {
+            console.log(ret);
+            _$$this.$data.tableData = ret.body;
+        }, (err) => {
+            console.log(err);
+            _$$this.$data.tableData = err.body;
+        }))
+      },
+
+      clear6() {
+        var _$$this = this;
+        _$$this.$data.tableData = [];
+      }
+    }
+  }
+</script>
+
+<style lang="postcss">
+  .index-card {
+    width: 800px;
+    margin: 100px auto;
+  }
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,141 @@
+const { join, resolve } = require('path')
+const webpack = require('webpack')
+const glob = require('glob')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin')
+
+const entries = {}
+const chunks = []
+glob.sync('./src/pages/**/app.js').forEach(path => {
+  const chunk = path.split('./src/pages/')[1].split('/app.js')[0]
+  entries[chunk] = path
+  chunks.push(chunk)
+})
+
+const config = {
+  entry: entries,
+  output: {
+    path: resolve(__dirname, './dist'),
+    filename: 'assets/js/[name].js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      assets: join(__dirname, '/src/assets'),
+      components: join(__dirname, '/src/components'),
+      root: join(__dirname, 'node_modules')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            css: ExtractTextPlugin.extract({
+              use: 'css-loader',
+              fallback: 'style-loader'
+            }),
+            postcss: ExtractTextPlugin.extract({
+              use: ['css-loader', 'postcss-loader'],
+              fallback: 'style-loader'
+            })
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader', 'postcss-loader'],
+          fallback: 'style-loader'
+        })
+      },
+      {
+        test: /\.html$/,
+        use: [{
+          loader: 'html-loader',
+          options: {
+            root: resolve(__dirname, 'src'),
+            attrs: ['img:src', 'link:href']
+          }
+        }]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2|svg|svgz)(\?.+)?$/,
+        exclude: /favicon\.png$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 10000
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new CommonsChunkPlugin({
+      name: 'vendors',
+      filename: 'assets/js/vendors.js',
+      chunks: chunks,
+      minChunks: chunks.length
+    }),
+    new ExtractTextPlugin({
+      filename: 'assets/css/[name].css',
+      allChunks: true
+    })
+  ],
+  devServer: {
+    host: '127.0.0.1',
+    port: 8010,
+    historyApiFallback: false,
+    noInfo: true,
+    proxy: {
+      '/api/*': {
+        target: 'http://0.0.0.0:3030'
+      }
+    }
+  },
+  devtool: '#eval-source-map'
+}
+
+glob.sync('./src/pages/**/*.html').forEach(path => {
+  const chunk = path.split('./src/pages/')[1].split('/app.html')[0]
+  const filename = chunk + '.html'
+  const htmlConf = {
+    filename: filename,
+    template: path,
+    inject: 'body',
+    favicon: './src/assets/img/logo.png',
+    hash: process.env.NODE_ENV === 'production',
+    chunks: ['vendors', chunk]
+  }
+  config.plugins.push(new HtmlWebpackPlugin(htmlConf))
+})
+
+module.exports = config
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ])
+}
