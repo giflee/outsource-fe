d0defb4139a50632ab6b5d25c7b3a832009fc031
hongjie <hongjie@corp.netease.com>
2017-07-10 20:23:18 +0800

    test

---
+++ src/pages/geely/geelypc/login/app.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import App from './app.vue'
+import vueResource from 'vue-resource'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+
+Vue.use(MintUI)
+Vue.use(ElementUI)
+Vue.use(vueResource)
+
+new Vue({
+	el: '#app',
+	render: h=> h(App),
+})
