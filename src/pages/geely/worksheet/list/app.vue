eb614bbbe21c78ba416df2ae6e4c85c4119a5bc8
jianfulee <jianfulee@hotmail.com>
2017-07-12 10:18:52 +0800

    懒加载分页

--- src/pages/geely/worksheet/list/app.vue
+++ src/pages/geely/worksheet/list/app.vue
@@ -1,30 +1,39 @@
 <template>
 	<div id="app"  class="g-main">
-		<el-table
-		:data="tableData"
-		@row-click="worksheetQuery">
-      		<el-table-column  label="创建时间"  width="100">
-      			<template scope="scope">
-      				<span>{{ scope.row.createTime | date}}</span>
-      				<span>{{ scope.row.createTime | time}}</span>
-      			</template>
-      		</el-table-column>
-      		<el-table-column label="工单号">
-      			<template scope="scope">
-      				<span>{{ scope.row.id }}</span>
-      			</template>
-      		</el-table-column>
-      		<el-table-column label="工单状态" width="80">
-      			<template scope="scope">
-      				<span>{{ scope.row.actionStatus | status}}</span>
-      			</template>
-      		</el-table-column>
-      		<el-table-column label="评价状态" width="80">
-      			<template scope="scope">
-      				<span>{{ scope.row.isEvaluated | evaluated}}</span>
-      			</template>
-      		</el-table-column> 
-		</el-table>
+		<div class="content">
+			<el-table
+			:data="tableData"
+			@row-click="worksheetQuery" v-infinite-scroll="loadMore" infinite-scroll-disabled="loading" infinite-scroll-distance="5">
+	      		<el-table-column  label="创建时间"  width="100">
+	      			<template scope="scope">
+	      				<span>{{ scope.row.createTime | date}}</span>
+	      				<span>{{ scope.row.createTime | time}}</span>
+	      			</template>
+	      		</el-table-column>
+	      		<el-table-column label="工单号">
+	      			<template scope="scope">
+	      				<span>{{ scope.row.id }}</span>
+	      			</template>
+	      		</el-table-column>
+	      		<el-table-column label="工单状态" width="80">
+	      			<template scope="scope">
+	      				<span>{{ scope.row.actionStatus | status}}</span>
+	      			</template>
+	      		</el-table-column>
+	      		<el-table-column label="评价状态" width="80">
+	      			<template scope="scope">
+	      				<span>{{ scope.row.isEvaluated | evaluated}}</span>
+	      			</template>
+	      		</el-table-column> 
+			</el-table>
+			<p v-show="loading" class="page-infinite-loading">
+		        <mt-spinner type="fading-circle"></mt-spinner>
+		        加载中...
+		    </p>
+		    <p v-show="allLoaded" class="page-infinite-loading">
+		        没有更多了
+		    </p>
+		</div>
 	</div>
 </template>
 
@@ -36,12 +45,17 @@ export default {
 		return {
 			tableData:[
 
-			]
+			],
+			loading: false,
+			current: 0,
+			limit: 15,
+			allLoaded: false,
+			pageNum: 0,
+			pageTotal:0
 		}
 	},
 	created: function() {
 			this.init();
-			this.getList();
 		},
 	methods: {
 		worksheetQuery(val) {
@@ -64,16 +78,36 @@ export default {
 			var urlObj = util.parseQueryString(location.search);
 			this.$data.mobile = urlObj.mobile;
 		},
-		getList() {
+		loadMore() {
 			var _$$this = this;
-			this.$http.get('/geely/api/worksheet/list', {
-				emulateJSON: true
-			}).then((_ret => {
-				if (_ret.body.code == 200) {
-					_.merge(_$$this.$data.tableData, _ret.body.result, true);
-					this.$forceUpdate();
-				}
-			}))
+			if (this.$data.current != 0) this.loading = true;
+			// 分页的页数
+			this.$data.pageNum = parseInt(this.$data.pageTotal/this.$data.limit);
+			this.$data.offset = (this.$data.limit)*(this.$data.current);
+			// 小于当前页
+			if (this.$data.current <= this.$data.pageNum) {
+				setTimeout(() => {
+					this.$http.get('/geely/api/worksheet/list', {
+						emulateJSON: true,
+						params: {
+							offset: this.$data.offset,
+							limit: _$$this.$data.limit
+						}
+					}).then((_ret => {
+						if (_ret.body.code == 200) {
+							_$$this.$data.tableData = _$$this.$data.tableData.concat(_ret.body.result);
+							// get total
+							this.$data.pageTotal = _ret.body.total;
+							this.loading = false;
+							this.$data.current = this.$data.current+1;
+							this.$forceUpdate();
+						}
+					}))
+				}, 500)
+			}else{
+				this.$data.allLoaded = true;
+				this.loading = false;
+			}
 		}
 	},
 	filters: {
@@ -115,4 +149,19 @@ export default {
 	white-space: nowrap;
     overflow: auto;
 } 
+.content{
+	position: absolute;
+    top: 0px;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    overflow-y: auto;
+    padding-bottom: 20px;
+}
+.page-infinite-loading{
+	text-align: center;
+}
+.mint-spinner-fading-circle{
+	margin: 8px auto;
+}
 </style>
